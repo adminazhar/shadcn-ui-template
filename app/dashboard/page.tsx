c4c8158a11a19666f9d3b179ee63534af5b6929d
adminazhar <admin@azhark.com>
2023-09-05 18:19:38 +0530

    template v1

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next"
+
+
+export const metadata: Metadata = {
+    title: "Dashboard",
+    description: "Example dashboard app built using the components.",
+}
+
+export default function Page() {
+    return (
+      <>
+      Yo
+      </>
+    );
+}
